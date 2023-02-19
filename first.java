d990a43d8723e63705110684de39974fce3412b8
Anjali625117 <104921358+Anjali625117@users.noreply.github.com>
2023-02-19 21:54:54 +0530

    added the 3rd change

--- first.java
+++ first.java
@@ -2,7 +2,7 @@ class first
 {
     public static void main(String[]max) {
         System.out.println("bfwbderwerf");
-        System.out.println("changed for good god make this work");
+        System.out.println("changed for good god make this work and the third change");
         
     }
 }
